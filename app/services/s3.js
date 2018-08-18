2a7a869ff1edea47a0fea804bbbcbe8f0e035a70
Minh Nguyen <tienminh.uet@gmail.com>
2018-08-18 10:08:18 +0700

    test

--- app/services/s3.js
+++ app/services/s3.js
@@ -15,7 +15,7 @@ function createS3SignedUrl({fileName, fileType, expired, acl, action}) {
         Key: fileName,
         Expires: expired || 600,
         ContentType: fileType,
-        ACL: acl || 'public-read'
+        ACL: acl
     };
 
     return new Promise((resolve, reject) => {
